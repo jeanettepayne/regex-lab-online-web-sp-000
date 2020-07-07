c16c99505b922eb58ab6b3f461669dec12379b9d
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-07 22:51:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,7 +27,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if phone.match(/[-() ]*\d{3,10}/)
+ if phone.match(/[(]/)
    true
  else
    false
